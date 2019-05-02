ee5e56658be7189311feaf743cad7dfd2e47b435
Emily Scott <ems17@hood.edu>
2019-05-02 19:43:44 -0400

    Finished Bar Graph
    I rounded the percentages.

--- SeqShinyApp.R
+++ SeqShinyApp.R
@@ -1,48 +1,170 @@
 library(shiny)
 library(plotrix)
+library(seqinr)
 
 ui<-fluidPage(
   
   # Input() functions,
+  helpText("Be sure to enter only DNA sequences containing the base pairs
+           A, T, C, and G for this particular Shiny App. When you click 'Submit Sequence', 
+           an analysis of the sequence will be displayed, including the total number
+           of base pairs, the percentage of each base pair type, and the amino acid 
+           sequence that the DNA codes for."),
+  
   textInput( inputId = "seq", 
              label = "Enter your FASTA sequence here:", 
-             value = "Enter FASTA sequence...", 
-             width = NULL, placeholder = NULL )
-  ,
+             value = "", 
+             width = NULL, placeholder = NULL ),
   
-
-  #Practice, I don't think we actually need this in our code. We need to calculate the seq length in our code for out users.
-  sliderInput(inputId = "inputSeq", 
-              label = "How long is your sequence?", 
-              value = 25, min = 1, max = 100)
+  # Addition of a submit-type button so user can decide when they are finish entering a sequence
+  submitButton("Submit Sequence", icon = NULL, width = NULL ),
   
-  ,
   # Output() functions
-  plotOutput(outputId = "pie"),
-  output$seq,
-  output$inputSeq
+  plotOutput(outputId = "bar"),
+  textOutput(outputId = "seq"),
+  textOutput(outputId = "length"),
+  textOutput(outputId =  "base"),
+  textOutput(outputId = "codons"),
+  textOutput(outputId = "amino_acids")
   
-)
+  )
 
-
-server <- function(input, output){
-  output$pie <- renderPlot({pie3D(slices, labels = basLabel, explode= 0.1, main = "Pie Chart of Bases")})
-}
-shinyApp(ui = ui, server = server)
-
-
-
-#Practice Pie Chart
-seq = cbind("A", "T", "G", "C")
-A <- sum(seq==("A"))
-T <- sum(seq==("T"))
-G <- sum(seq==("G"))
-C <- sum(seq==("C"))
-slices <- c(A, T, G, C)
-basLabel <- c("A", "T", "G", "C")
-baspct <- round(slices/sum(slices)*100)
-basLabel <- paste(basLabel, baspct) #add percent to base labels
-basLabel <- paste(basLabel, "%", sep ="") #add % to labels
-print(pie3D(slices, labels = basLabel, explode= 0.1, main = "Pie Chart of Bases"))
-
+server <- function(input, output)
+{
+  
+  # Count the number of base pairs entered by the user 
+  output$length <- renderPrint({nchar(input$seq)})
+  
+  # Count the number of each base pair (A, T, C, G) entered by the user
+  
+  output$base <- renderPrint(
+    {
+      for (base in input$seq)
+      {
+        A <- s2c(input$seq)
+        A_sum <- sum(A == "A")
+        T_sum <- sum(T == "T")
+        G_sum <- sum(G == "G")
+        C_sum <- sum(C == "C")
+        slices <- c(A_sum, T_sum, G_sum, C_sum)
+        basLabel <- c("A", "T", "G", "C")
+        baspct <- round(slices/sum(slices)*100)
+        basLabel <- paste(basLabel, baspct) #add percent to base labels
+        basLabel <- paste(basLabel, "%", sep ="") #add % to labels
+        
+        
+        
+      } 
+      print( A_sum)
+      print( T_sum)
+      print( G_sum)
+      print( C_sum)
       
+    }
+  )
+  # Will display the sequence entered by the user
+  output$seq <- renderPrint({input$seq})
+  
+  # Code to create a pie chart of the bases
+  output$bar <- renderPlot({  
+                            basLabel <- c("A", "T", "G", "C")
+                            A <- s2c(input$seq)
+                            A_sum <- sum(A == "A")
+                            T_sum <- sum(A == "T")
+                            G_sum <- sum(A == "G")
+                            C_sum <- sum(A == "C")
+                            seq_sum<-sum(A_sum, T_sum, G_sum, C_sum)
+                            percent <- cbind(A_percent <- round(A_sum/seq_sum*100),
+                                             T_percent <- round(T_sum/seq_sum*100),
+                                             G_percent <- round(G_sum/seq_sum*100),
+                                             C_percent <- round(C_sum/seq_sum*100))
+                            basLabel <- paste(basLabel, percent, "%") #add % to labels
+                            print(barplot(height = percent,
+                                          beside = TRUE,
+                                          width = 1, 
+                                          legend.text = basLabel,
+                                          col = c("red", "blue", "yellow", "black"),
+                                          args.legend = list(x="topleft"),
+                                          ylab = "Percentage (%)",
+                                          border = "dark blue",
+                                          main = "Bar Graph of Bases"))
+    
+  })
+  
+  # Code for splitting into codons
+  # Splits up sequences into groups of three base pairs (i.e. codons)
+  output$codons <- renderText({
+    
+    # Installed the "seqinr" package to split sequence up into codons.
+    input <- s2c(input$seq) # Store user input sequence as a variable # MDG for example to aa
+    # s2c is a utility function used to convert string into characters
+    sequence <- splitseq(seq= input, frame = 0 , word= 3)
+  })
+  # Code for assigning contains to an amino acid; code for putting together aa sequence
+  
+  output$amino_acids <- renderText({
+    
+      get_aa_sequence <- function(sequence= sequence){
+      
+      amino_acid_code_codon_chart <- {
+        'TTT'='F'; 'TTC'='F'; 'TTA'='L'; 'TTG'='L'; 'TCT'='S';
+        'TCC'='S'; 'TCA'='S'; 'TCG'='S'; 'TAT'='Y'; 'TAC'='Y';
+        'TGT'='C'; 'TGC'='C'; 'TGG'='W'; 'CTT'='L'; 'CTC'='L';
+        'CTA'='L'; 'CTG'='L'; 'CCT'='P'; 'CCC'='P'; 'CCA'='P';
+        'CCG'='P'; 'CAT'='H'; 'CAC'='H'; 'CAA'='Q'; 'CAG'='Q';
+        'CGT'='R'; 'CGC'='R'; 'CGA'='R'; 'CGG'='R'; 'ATT'='I';
+        'ATC'='I'; 'ATA'='I'; 'ATG'='M'; 'ACT'='T'; 'ACC'='T';
+        'ACA'='T'; 'ACG'='T'; 'AAT'='N'; 'AAC'='N'; 'AAA'='K';
+        'AAG'='K'; 'AGT'='S'; 'AGC'='S'; 'AGA'='R'; 'AGG'='R';
+        'GTT'='V'; 'GTC'='V'; 'GTA'='V'; 'GTG'='V'; 'GCT'='A';
+        'GCC'='A'; 'GCA'='A'; 'GCG'='A'; 'GAT'='D'; 'GAC'='D';
+        'GAA'='E'; 'GAG'='E'; 'GGT'='G'; 'GGC'='G'; 'GGA'='G';
+        'GGG'='G'}
+      
+      stop_codons <- list('TAA', 'TAG', 'TGA')
+      
+      aa_sequence <- " " # Initialize variable to store amino acids as they are added to the sequence
+      for (item in sequence)
+      {
+        
+        
+        aa_sequence <- aa_sequence + amino_acid_code_codon_chart.get(item)
+        print(aa_sequence)
+        
+      }
+    }
+    
+  }
+  )
+  #Practice Pie Chart
+  seq = cbind("A", "T", "G", "C", "T")
+  seq_sum <- 5
+  A_sum <- sum(seq==("A"))
+  T_sum <- sum(seq==("T"))
+  G_sum <- sum(seq==("G"))
+  C_sum <- sum(seq==("C"))
+  slices <- c(A, T, G, C)
+  basLabel <- c("A", "T", "G", "C")
+  baspct <- round(slices/sum(slices)*100)
+  
+  #Practice Bar Graph
+  basLabel <- c("A", "T", "G", "C")
+  percent <- cbind(A_percent <- (A_sum/seq_sum*100),
+                   T_percent <- (T_sum/seq_sum*100),
+                   G_percent <- (G_sum/seq_sum*100),
+                   C_percent <- (C_sum/seq_sum*100))
+  basLabel <- paste(basLabel, percent, "%") #add % to labels
+  print(barplot(height = percent,
+               beside = TRUE,
+               width = 1, 
+               legend.text = basLabel,
+               col = c("red", "blue", "yellow", "black"),
+               args.legend = list(x="topleft"),
+               ylab = "Percentage (%)",
+               border = "dark blue",
+               main = "Bar Graph of Bases"))
+  
+}    
+
+
+shinyApp(ui = ui, server = server)
