00f86f4529ba36ab5476dd24f9cd4999d649d4d7
Alyssa <amk19@hood.edu>
2019-05-08 12:55:30 -0400

    Updated observeEvent() 2.0
    Still doesn't work but is getting there

--- SeqShinyApp.R
+++ SeqShinyApp.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(plotrix)
 library(seqinr)
 
 ui<-fluidPage(
@@ -8,9 +7,14 @@ ui<-fluidPage(
            A, T, C, and G for this particular Shiny App. When you click 'Submit Sequence', 
            an analysis of the sequence will be displayed, including the total number
            of base pairs, the percentage of each base pair type, and the amino acid 
-           sequence that the DNA codes for."),
+           sequence that the DNA codes for. Be warned, your sequence will be striped of remaining bases if 
+           sequence provided is not divisible by three."),
   
   # Input() functions,
+  
+  tags$style(type="text/css",
+             ".shiny-output-error { visibility: hidden; }",
+             ".shiny-output-error:before { visibility: hidden; }"),
   
   textInput( inputId = "seq", 
              label = "Enter your FASTA sequence here:", 
@@ -18,7 +22,7 @@ ui<-fluidPage(
              width = NULL, placeholder = NULL ),
   
   # Addition of a submit-type button so user can decide when they are finish entering a sequence
-  submitButton("Submit"),
+  actionButton("button","Submit"),
   
   # Output() functions
   plotOutput(outputId = "bar"),
@@ -32,15 +36,51 @@ ui<-fluidPage(
 
 server <- function(input, output)
 {
-  seq_input <- eventReactive(input$Submit, {runif(input$seq)})
-  #seq_input <- reactiveVal('')
-  #observeEvent(input$seq,{seq_input()})
+  seq_input <- reactiveVal('')
+  base_input <- reactiveVal(0)
+  observeEvent(input$button,{(
+    
+    seq_input(
+      toupper(input$seq),
+      tmp<-c("A", "T", "G", "C"), # Creates list of A,T,G,C
+      tmp <- table(tmp), # Makes into a table with four different columns A, T, G, C
+      tmp[]<-0, # Sets all base values to zero
+      base_numbers <- strsplit(seq_input, ""), # Splits up input sequence into individual characters
+      base_numbers <- unlist(base_numbers), # Unlists the base_numbers variable
+      base_numbers <- table(base_numbers),  # Makes variable into a table
+      tmp[] <- base_numbers, # Does not work- need to append tmp table with base_numbers
+      tmp_per<-tmp/((sum(tmp))*100),
+      TMP<-c("Number", tmp),
+      tmp_PER<-c("Percent", tmp_per),
+      tmp_total<-rbind((TMP), (tmp_PER))
+    )
+  )
+    
+    
+    base_input(
+      #the percentages of base pairs shown in graph
+      percent <- (tmp_per),
+      
+      #the label for the bar graph
+      bases <- c("A", "T", "G", "C"),
+      basLabel <- paste(bases, tmp_per, "%")
+    )
+  })
+  
+  # Will display the sequence entered by the user
+  output$seq <- renderPrint({
+    
+    print(paste("This is the sequence you entered:" , seq_input()))
+    
+    })
   
   # Count the number of base pairs entered by the user 
-  output$length <- renderPrint({nchar(seq_input)
+  output$length <- renderPrint({
+    
+    print(paste("The number of bases in the sequence entered is:", nchar(seq_input())))
     if(seq_input() == '')
     {
-      ""
+      print("NA")
     }else{
       nchar(seq_input())
     }
@@ -48,19 +88,31 @@ server <- function(input, output)
   
   # Count the number of each base pair (A, T, C, G) entered by the user
   
+  #Produce an error message if the sequence does not make sense
+  output$error <- renderText({
+    A <- ((seq_input()))
+    if (any((A != "A") & (A != "T") & (A != "G") & (A != "C"))) {
+      print("You need to enter only FASTA formatted DNA base pairs A, T, G, and C, please try again")
+    } else {
+      print("Thank you!")
+    }
+    
+  })
+  
   output$base <- renderPrint(
       {
         if(seq_input() == '')
         {
-          ""
+          ''
         }else{
-          #base_numbers <- table(seq_input)
-          base_numbers <- strsplit(seq_input, "")
-          base_numbers <- unlist(base_numbers)
-            print(sum(base_numbers =="A"))
-            print(sum(base_numbers =="T"))
-            print(sum(base_numbers =="G"))
-            print(sum(base_numbers =="C"))
+          #base_numbers <- strsplit(seq_input, "")
+          #base_numbers <- unlist(base_numbers)
+            #sum_A <- sum(base_numbers =="A")
+            #print(paste("The number of adenine in the sequence is:" , sum_A))
+            #print(paste("The number of thymine in the sequence is:", sum_T))
+            #print(paste("The number of guanine in the sequence is:", sum_G))
+            #print(paste("The number of guanine in the sequence is:", sum_C))
+            #print(sum(base_numbers =="C"))
         }
       })
      # for (base in seq_input)
@@ -75,14 +127,11 @@ server <- function(input, output)
       #print(T_sum)
       #print(G_sum)
      # print(C_sum)
-      
-  # Will display the sequence entered by the user
-  output$seq <- renderPrint({toupper(input$seq)})
   
   # Code to create a bar graph of the bases
   output$bar <- renderPlot({  
                             basLabel <- c("A", "T", "G", "C")
-                            A <- s2c(input$seq)
+                            A <- s2c(seq_input())
                             A_sum <- sum(A == "A")
                             T_sum <- sum(A == "T")
                             G_sum <- sum(A == "G")
@@ -105,12 +154,15 @@ server <- function(input, output)
     
   })
   
+  # Will display the sequence entered by the user
+  output$seq <- renderPrint({seq_input()})
+  
   # Code for splitting into codons
   # Splits up sequences into groups of three base pairs (i.e. codons)
   output$codons <- renderText({
     
     # Installed the "seqinr" package to split sequence up into codons.
-    input <- s2c(toupper(input$seq)) # Store user input sequence as a variable # MDG for example to aa
+    input <- s2c(toupper(seq_input())) # Store user input sequence as a variable # MDG for example to aa
     # s2c is a utility function used to convert string into characters
     sequence <- splitseq(seq= input, frame = 0 , word= 3)
   })
@@ -118,11 +170,10 @@ server <- function(input, output)
   
   output$amino_acids <- renderText({
     
-    input <- s2c(input$seq)
+    input <- s2c(seq_input())
     sequence <- splitseq(seq= input, frame = 0 , word= 3)
-    amino_acid <- getGeneticCode()(sequence)
+    amino_acid <- getGeneticCode()[(sequence)]
     print(amino_acid)
-    # get aa_sequence
  } )
   
  
